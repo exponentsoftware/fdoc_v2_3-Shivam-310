7cb679dd9bbdb34143cbe7260fc209123beef406
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-05-19 14:42:24 +0530

    initial commit

---
+++ 1.js
@@ -0,0 +1,38 @@
+const companies = [
+    {
+      name: 'Company 1',
+      employees: [
+        { name: 'Alice', role: 'Developer', department: 'Engineering' },
+        { name: 'Bob', role: 'Manager', department: 'Engineering' },
+        { name: 'Charlie', role: 'Designer', department: 'Design' },
+      ],
+    },
+    // ... (more companies)
+  ];
+  
+  function processCompanies(companies) {
+    return companies.map((company) => {
+      const numEmployees = company.employees.length;
+      const departments = {};
+  
+      company.employees.forEach((employee) => {
+        const department = employee.department;
+  
+        if (departments.hasOwnProperty(department)) {
+          departments[department]++;
+        } else {
+          departments[department] = 1;
+        }
+      });
+  
+      return {
+        ...company,
+        numEmployees,
+        departments,
+      };
+    });
+  }
+  
+  const updatedCompanies = processCompanies(companies);
+  console.log(updatedCompanies);
+  
